3d09901516de459eb0747d80ab5e92e9ae8291c7
Qwark <givernaud.f@gmail.com>
2015-01-06 13:47:33 +0100

    first

--- db.sh
+++ db.sh
@@ -1,4 +1,2 @@
 key1 val1
-key2 val2
-key3 val2
-key3 val1
+key2 val9
--- test.sh
+++ test.sh
@@ -28,30 +28,30 @@ case $1 in
 	    if [[ $key = \$* ]];
 	    then
 		key=${key:1}
-		tst=`grep $key db.sh`
+		tst=`grep ^$key db.sh`
 		if [[ -z $tst ]];
 		then
 		    echo "No such key : $key"
 		    exit 0
 		else
-		    key=`grep $key db.sh`
+		    key=`grep ^$key db.sh`
 		    key=${key#* }
 		fi
 	    fi
 	    if [[ $val = \$* ]];
 	    then
 		val=${val:1}
-		tst=`grep $val db.sh`
+		tst=`grep ^$val db.sh`
 		if [[ -z $tst ]];
 		then
 		    echo "No such key : $val"
 		    exit 0
 		else
-		    val=`grep $val db.sh`
+		    val=`grep ^$val db.sh`
 		    val=${val#* }
 		fi
 	    fi
-	    tmp=`grep "^$key*" db.sh`
+	    tmp=`grep "^$key" db.sh`
 	    tmp=${tmp% *};
 	    if [[ -z $tmp ]];
 	    then
@@ -60,9 +60,7 @@ case $1 in
 	    then
 		echo $key $val >> db.sh
 	    else
-		sed "/^$key*/c $key $val" db.sh > tmp
-		rm db.sh
-		mv tmp db.sh
+		sed -i "/^$key/c $key $val" db.sh
 	    fi
 	fi;;
     "del" ) echo "You choosed del";;
